d9fe53dd8070efbd6261eebce063cbc4adfd34a8
airondevt <airondev@gmail.com>
2023-08-22 13:56:59 +0100

    laravel 10 support

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CodebarAg\TwilioVerify\Tests;
+
+use CodebarAg\TwilioVerify\TwilioVerifyServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Spatie\\TwilioVerify\\Database\\Factories\\' . class_basename($modelName) . 'Factory',
+        );
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            TwilioVerifyServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app): void
+    {
+        config()->set('database.default', 'testing');
+    }
+}
